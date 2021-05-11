7fca5a6564de4027f04ad5f0e0dedc63f9bdef94
“[Matti <[matti.tamminen@codecontrol.fi]>
2021-05-11 14:34:46 +0300

    finished

---
+++ app.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws')
+const http = require('http')
+const { readFileSync } = require('fs')
+
+const homePage = readFileSync('./index.html')
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(homePage)
+    res.end()
+})
+
+const PORT = 5000
+
+const socket = new WebSocket.Server({ server })
+
+socket.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        socket.clients.forEach( function each(client) {
+            if (client != ws && client.readyState == WebSocket.OPEN) {
+                client.send(data)
+            }
+        })
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
